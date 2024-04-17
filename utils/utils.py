5b70616021ee47eaa6f79cafa75b800b3618878d
BetterCallAndrulek <156514407+BetterCallAndrulek@users.noreply.github.com>
2024-04-17 18:59:01 +0700

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -3,57 +3,95 @@ from utils.config import config
 from classes.HHParser import HHParser
 
 
-def create_database(db_name):
-    conn = psycopg2.connect(dbname="postgres", **config())
+def get_hh_employers() -> list:
+    """Возвращает список данных о 10 работодателях с hh.ru"""
+    url = "https://api.hh.ru/employers"
+    params = {'only_with_vacancies': True,
+              'per_page': 10}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        response_data = json.loads(response.text)
+        return response_data['items']
+    raise ConnectionError
+
+
+def get_hh_data(employers: list) -> list:
+    """Возващает список данных о вакансиях по выбранным
+    работодателям с hh.ru"""
+    url = "https://api.hh.ru/vacancies"
+    vacancies_data = []
+    for employer in employers:
+        params = {'employer_id': employer['id']}
+        response = requests.get(url, params)
+        response_data = json.loads(response.text)
+        vacancies_data.extend(response_data['items'])
+    return vacancies_data
+
+
+def create_database(params: dict, db_name: str) -> None:
+    """Создает новую базу данных"""
+    conn = psycopg2.connect(**params)
     conn.autocommit = True
     cur = conn.cursor()
-    cur.execute(f'DROP DATABASE IF EXISTS {db_name}')
-    cur.execute(f'CREATE DATABASE {db_name}')
-
-    cur.close()
+    cur.execute(sql.SQL('CREATE DATABASE {};').format(
+        sql.Identifier(db_name)))
     conn.close()
 
 
-def create_tables(db_name):
-    conn = psycopg2.connect(dbname="db_name", **config())
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute('''CREATE TABLE employers 
-                           (employer_id int PRIMARY KEY,
-                           name VARCHAR(255) UNIQUE NOT NULL,
-                           open_vacancies int
-                           );
-                           ''')
-            cur.execute('''CREATE TABLE vacancies
-                        (vacancy_id int,
-                        name VARCHAR(255) NOT NULL,
-                        salary_from int,
-                        salary_to int,
-                        url VARCHAR(255),
-                        employer_id int REFERENCES employers(employer_id) NOT NULL
-                        );
-                        ''')
-    conn.close()
+def create_employers_table(cur) -> None:
+    """Создает таблицу employers"""
+    cur.execute('''
+        CREATE TABLE employers (
+            id INTEGER PRIMARY KEY,
+            name varchar(100) NOT NULL,
+            open_vacancies int
+    )
+    ''')
 
 
-def insert_data_into_tables(db_name):
-    hh = HHParser()
-    employers = hh.get_employers()
-    vacancies = hh.filter_vacancies()
-    conn = psycopg2.connect(dbname='db_name', **config())
-    with conn:
-        with conn.cursor() as cur:
-            for employer in employers:
-                cur.execute("""
-                                INSERT INTO employers VALUES (%s, %s)
-                            """, (employer["id"], employer["name"]))
-            for vacancy in vacancies:
-                cur.execute("""INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s, %s)
-                                    """, (vacancy["id"], vacancy["name"],
-                                          vacancy["salary_from"], vacancy["salary_to"],
-                                          vacancy["url"], vacancy["employer"]))
-    conn.close()
+def create_vacancies_table(cur) -> None:
+    """Создает таблицу vacancies"""
+    cur.execute('''
+        CREATE TABLE vacancies (
+            id INTEGER PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            salary_from INTEGER,
+            salary_to INTEGER,
+            currency VARCHAR(10),
+            url VARCHAR(100) NOT NULL,
+            employer_id INTEGER NOT NULL,
+            FOREIGN KEY (employer_id) REFERENCES employers (id)
+    )
+    ''')
 
-create_database("db_name")
-create_tables("db_name")
-insert_data_into_tables("db_name")
+
+def insert_employers_data(cur, employers: list) -> None:
+    """Заполняет таблицу employers данными"""
+    for employer in employers:
+        query = "INSERT INTO employers VALUES (%s, %s, %s)"
+        employer_data = (int(employer['id']),
+                         employer['name'],
+                         employer['open_vacancies'])
+        cur.execute(query, employer_data)
+
+
+def insert_vacancies_data(cur, vacancies: list) -> None:
+    """Заполняет таблицу vacancies данными"""
+    for vacancy in vacancies:
+        query = "INSERT INTO vacancies VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        try:
+            vacancy_data = (int(vacancy['id']),
+                            vacancy['name'],
+                            vacancy['salary']['from'],
+                            vacancy['salary']['to'],
+                            vacancy['salary']['currency'],
+                            vacancy['alternate_url'],
+                            int(vacancy['employer']['id']))
+        except TypeError:
+            vacancy_data = (int(vacancy['id']),
+                            vacancy['name'],
+                            None, None, None,
+                            vacancy['alternate_url'],
+                            int(vacancy['employer']['id']))
+
+        cur.execute(query, vacancy_data)
